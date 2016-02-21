b137d476db544949315fafbc2c3fb9b41d90f9a7
Steven Noto <snoto@simplyautomatic.com>
2016-02-21 03:22:00 -0600

    Added more boilerplate code.

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+    require __DIR__ . '/vendor/autoload.php';
 
-    require __DIR__ . '/../vendor/autoload.php';
+    use \Psr\Http\Message\ServerRequestInterface as Request;
+    use \Psr\Http\Message\ResponseInterface as Response;
 
+    $app = new \Slim\App;
+    
+    // Samples
+    $app->get('/', function (Request $request, Response $response) {
+        $response->getBody()->write("Hello world");
+        return $response;
+    });
+    $app->get('/hello/{name}', function (Request $request, Response $response) {
+        $name = $request->getAttribute('name');
+        $response->getBody()->write("Hello, $name");
+        return $response;
+    });
+    
+    // Prototypes
+    $app->get('/api/locations/{token}', function (Request $request, Response $response) {
+        $token = $request->getAttribute('token');
+        $response->getBody()->write("You asked to get location $token");
+        return $response;
+    });
+    $app->post('/api/locations/', function (Request $request, Response $response) {
+        $token = bin2hex(openssl_random_pseudo_bytes(16));
+        $tokenlength = strlen($token); // 32
+        $response->getBody()->write("You did a post, here's a token: $token");
+        return $response;
+    });
+//    $app->config('debug', true);
+    $app->run();
 ?>
